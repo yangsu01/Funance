50445f22f4ed15e9b6f57920d25f9e01417c9bc5
yangsu01 <ssgc1101@gmail.com>
2024-04-19 17:36:27 -0700

    update page content

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,32 +1,41 @@
-import Button from "../components/Button";
+// components
+import Title from "../components/Title";
 
 const Home = () => {
+  const title = "Welcome to Funance";
+  const subTitle = "Investing is fun!";
   return (
-    <div className="px-4 py-5 text-center">
-      <h1 className="display-5 fw-bold text-white">
-        <strong>Welcome to Funance</strong>
-      </h1>
+    <div className="home-container">
+      <Title title={title} subTitle={subTitle} />
 
-      <div className="col-lg-6 mx-auto mt-5">
-        <p className="fs-5">
-          Join me on my journey of learning about quantitative finance and check
-          out my latest blog posts uploaded on{" "}
+      <p className="fs-5">
+        Join me on my journey of learning about everything finance, especially
+        the quantitative side of it! Be sure to Check out my latest{" "}
+        <strong>Weekly blog</strong> posts uploaded on{" "}
+        <strong>
           <a className="text-white" href="/blog">
-            Funance Blog.
-          </a>{" "}
-          Updated weekly!
-        </p>
-
-        <p className="fs-5">
-          Or, create/join a lobby and start trading in the portfolio simulator.
-          Compete against other players to see who can make the most money in a
-          set amount of time! All prices are based on real-time stock data. To
-          get started, check out the{" "}
-          <a className="text-white" href="/blog">
-            game rules.
+            Funance Blog
           </a>
-        </p>
-      </div>
+        </strong>{" "}
+        where I explore and learn about different applications of math and
+        datascience in finance and investing.
+      </p>
+
+      <br />
+
+      <p className="fs-5">
+        Want to test the waters or try out a new investing strategy? Create/join
+        a lobby and start trading in the <strong>portfolio simulator</strong>:
+        Compete against others (or yourself) to see who can make the most money
+        in a set amount of time! All prices are based on real-time stock data.
+        check out the{" "}
+        <strong>
+          <a className="text-white" href="/game-rules">
+            game rules
+          </a>
+        </strong>{" "}
+        to get started.
+      </p>
     </div>
   );
 };
