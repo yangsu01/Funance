01987f912cf40fdc546cd1b954f9c7efb1347667
yangsu01 <ssgc1101@gmail.com>
2024-04-23 21:48:15 -0700

    feat: add optional button

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -1,16 +1,35 @@
+import { Row, Col, Button } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
+
 type Props = {
   title: string;
-  subTitle: string;
+  subtitle: string;
+  button?: string;
+  onClick?: () => void;
 };
 
 const Title = (props: Props) => {
-  const { title, subTitle } = props;
+  const { title, subtitle, button, onClick } = props;
+  const isSmallScreen = useMediaQuery({ maxWidth: 767 });
 
   return (
-    <div className="border-bottom mb-4">
-      <h1 className="display-5 fw-bold text-white">{title}</h1>
-      <h5>{subTitle}</h5>
-    </div>
+    <Row className="d-flex align-items-center border-bottom mb-4">
+      <Col sm={12} md={9}>
+        <h1 className="display-5 fw-bold text-white">{title}</h1>
+        <h5>{subtitle}</h5>
+      </Col>
+      {button && (
+        <Col
+          sm={12}
+          md={3}
+          className={isSmallScreen ? "my-3" : "d-flex justify-content-end"}
+        >
+          <Button variant="outline-light" size="lg" onClick={onClick}>
+            {button}
+          </Button>
+        </Col>
+      )}
+    </Row>
   );
 };
 
