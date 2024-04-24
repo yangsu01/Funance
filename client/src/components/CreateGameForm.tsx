972c003517e61db8de6b1cb9597272a331a5d967
yangsu01 <ssgc1101@gmail.com>
2024-04-23 21:44:07 -0700

    feat: add game creation form

---
+++ client/src/components/CreateGameForm.tsx
@@ -0,0 +1,208 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Form,
+  FloatingLabel,
+  Row,
+  Col,
+  InputGroup,
+  Container,
+} from "react-bootstrap";
+
+// types
+import { CreateGameFormData } from "../utils/types";
+
+type Props = {
+  onSubmit: (formData: CreateGameFormData) => void;
+};
+
+const CreateGameForm = (props: Props) => {
+  const { onSubmit } = props;
+  const [formData, setFormData] = useState<CreateGameFormData>({
+    name: "",
+    password: "",
+    startDate: "",
+    endDate: "",
+    startingCash: 0,
+    transactionFee: 0,
+    feeType: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate(-1);
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  useEffect(() => {
+    if (formData.feeType) {
+      setFormData({ ...formData, transactionFee: 0 });
+    }
+  }, [formData.feeType]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    onSubmit(formData);
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Container className="mb-4">
+          {/* game name and password */}
+          <Row>
+            <Col md={6} className="mb-4">
+              <Form.Label>Enter unique game name</Form.Label>
+              <FloatingLabel controlId="name" label="Game Name">
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Enter game name"
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
+              </FloatingLabel>
+            </Col>
+            <Col md={6} className="mb-4">
+              <Form.Label>Enter password (optional)</Form.Label>
+              <InputGroup>
+                <FloatingLabel controlId="password" label="Password">
+                  <Form.Control
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter password"
+                    onChange={(e) =>
+                      setFormData({ ...formData, password: e.target.value })
+                    }
+                  />
+                </FloatingLabel>
+                <InputGroup.Checkbox
+                  aria-label="show password"
+                  onClick={handleTogglePassword}
+                />
+              </InputGroup>
+            </Col>
+          </Row>
+
+          {/* start and end dates */}
+          <Row>
+            <Col md={6} className="mb-4">
+              <Form.Label>Enter game start date</Form.Label>
+              <FloatingLabel controlId="start date" label="Start Date">
+                <Form.Control
+                  required
+                  type="date"
+                  placeholder="Enter start date"
+                  onChange={(e) =>
+                    setFormData({ ...formData, startDate: e.target.value })
+                  }
+                />
+              </FloatingLabel>
+            </Col>
+            <Col md={6} className="mb-4">
+              <Form.Label>Enter game end date (optional)</Form.Label>
+              <FloatingLabel controlId="end date" label="End Date">
+                <Form.Control
+                  type="date"
+                  placeholder="Enter end date"
+                  onChange={(e) =>
+                    setFormData({ ...formData, endDate: e.target.value })
+                  }
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+
+          {/* starting cash and transaction fee */}
+          <Row>
+            <Col md={4} className="mb-4">
+              <Form.Label>Enter starting cash</Form.Label>
+              <InputGroup>
+                <InputGroup.Text>$</InputGroup.Text>
+                <FloatingLabel controlId="starting cash" label="Starting Cash">
+                  <Form.Control
+                    required
+                    type="number"
+                    placeholder="10000"
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        startingCash: Number(e.target.value),
+                      })
+                    }
+                  />
+                </FloatingLabel>
+              </InputGroup>
+            </Col>
+            <Col md={4} className="mb-4">
+              <Form.Label>Select transaction fee type</Form.Label>
+              <FloatingLabel controlId="fee type" label="Fee Type">
+                <Form.Select
+                  aria-label="feeType select"
+                  onChange={(e) =>
+                    setFormData({ ...formData, feeType: e.target.value })
+                  }
+                >
+                  <option value="">No Fee</option>
+                  <option value="Flat Fee">Flat Fee</option>
+                  <option value="Percentage">Percentage</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+            {formData.feeType && (
+              <Col md={4} className="mb-4">
+                <Form.Label>Enter transaction fee</Form.Label>
+                <InputGroup>
+                  {formData.feeType === "Flat Fee" && (
+                    <InputGroup.Text>$</InputGroup.Text>
+                  )}
+                  <FloatingLabel
+                    controlId="transaction fee"
+                    label="Transaction Fee"
+                  >
+                    <Form.Control
+                      type="number"
+                      placeholder="0"
+                      onChange={(e) =>
+                        setFormData({
+                          ...formData,
+                          transactionFee: Number(e.target.value),
+                        })
+                      }
+                    />
+                  </FloatingLabel>
+                  {formData.feeType === "Percentage" && (
+                    <InputGroup.Text>%</InputGroup.Text>
+                  )}
+                </InputGroup>
+              </Col>
+            )}
+          </Row>
+        </Container>
+
+        <Container className="d-flex justify-content-end">
+          <Button
+            variant="outline-light"
+            size="lg"
+            className="me-3"
+            onClick={handleCancel}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" variant="success" size="lg">
+            Create Game
+          </Button>
+        </Container>
+      </Form>
+    </>
+  );
+};
+
+export default CreateGameForm;
---
+++ client/src/pages/CreateGame.tsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// components
+import Title from "../components/Title";
+import CreateGameForm from "../components/CreateGameForm";
+
+import api from "../utils/api";
+
+// types
+import { CreateGameFormData } from "../utils/types";
+
+type Props = {
+  token: string | null;
+  showAlert: (message: string, type: "success" | "danger" | "warning") => void;
+};
+
+const CreateGame = (props: Props) => {
+  const { token, showAlert } = props;
+
+  // page title
+  const title = "Create Game";
+  const subtitle = "Create a new game with custom rules!";
+  const buttonText = "Game List";
+
+  const navigate = useNavigate();
+
+  const handleGameList = () => {
+    navigate("/game-list");
+  };
+
+  const handleCreateGame = (formData: CreateGameFormData) => {
+    const startDate = new Date(formData.startDate);
+    const currentDate = new Date();
+
+    // offset start date by 1 day cause its behind by 1 day for some reason??
+    const startDateNum = startDate.setDate(startDate.getDate() + 1);
+    const currentDateNum = currentDate.setDate(currentDate.getDate());
+
+    // handle empty variables
+    // if (formData.feeType === "") {
+    //   formData.feeType = "";
+    // }
+    if (!formData.endDate) {
+      delete formData.endDate;
+    }
+    if (!formData.password) {
+      delete formData.password;
+    }
+
+    // form validation
+    if (startDateNum <= currentDateNum) {
+      showAlert("Start date cannot be in the past!", "danger");
+      return;
+    }
+
+    if (formData.endDate) {
+      const endDate = new Date(formData.endDate);
+
+      if (endDate <= startDate) {
+        showAlert("End date must be after start date!", "danger");
+        return;
+      }
+    }
+
+    if (formData.startingCash <= 0) {
+      showAlert("Starting cash is too damn low!", "danger");
+      return;
+    }
+
+    if (formData.transactionFee < 0) {
+      showAlert("Transaction fee cannot be negative!", "danger");
+      return;
+    }
+
+    postCreateGame(formData);
+  };
+
+  const postCreateGame = async (formData: CreateGameFormData) => {
+    try {
+      const response = await api.post(
+        "/create-game",
+        {
+          gameName: formData.name,
+          gamePassword: formData.password,
+          gameStartDate: formData.startDate,
+          gameEndDate: formData.endDate,
+          startingCash: formData.startingCash,
+          transactionFee: formData.transactionFee,
+          feeType: formData.feeType,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      showAlert(response.data.msg, "success"); // change to navigate to game portfolio?
+    } catch (error: any) {
+      if (error.response && error.response.status === 409) {
+        showAlert("Game name already taken!", "warning");
+      } else {
+        showAlert("Cannot create game, please try again", "danger");
+      }
+    }
+  };
+
+  return (
+    <>
+      <Title
+        title={title}
+        subtitle={subtitle}
+        button={buttonText}
+        onClick={handleGameList}
+      ></Title>
+
+      <CreateGameForm onSubmit={handleCreateGame}></CreateGameForm>
+    </>
+  );
+};
+
+export default CreateGame;
