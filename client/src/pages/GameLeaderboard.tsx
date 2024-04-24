9301f6d99c7479c047aa8322b0f50cb490290072
yangsu01 <ssgc1101@gmail.com>
2024-04-23 21:49:09 -0700

    initial commit

---
+++ client/src/pages/GameLeaderboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GameLeaderboard = () => {
+  return <div>GameLeaderboard</div>;
+};
+
+export default GameLeaderboard;
---
+++ client/src/pages/MyPortfolio.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyPortfolio = () => {
+  return <div>MyPortfolio</div>;
+};
+
+export default MyPortfolio;
