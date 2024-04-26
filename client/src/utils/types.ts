a94392c128f01792ba1cca9564f47f1d3efbd4c1
yangsu01 <ssgc1101@gmail.com>
2024-04-25 18:35:00 -0700

    refactor: add types for game leaderboard data

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -28,3 +28,53 @@ export type AlertMessage = {
     alert: string;
     alertType: "success" | "danger" | "warning";
 }
+
+// game details
+export type GameDetails = {
+    name: string
+    creator: string
+    participants: string
+    startDate: string
+    endDate: string
+    startingCash: string
+    transactionFee: string
+    feeType: string
+    joinedGame: boolean
+    passwordRequired: boolean
+    lastUpdated: string
+    gameDuration: string
+    status: string
+}
+
+// closing history of a portfolio
+export type ClosingHistory = {
+    x: string[];
+    y: number[];
+    name: string
+}
+
+// daily history of a portfolio
+export type DailyHistory = {
+    x: string[];
+    y: number[];
+    name: string
+}
+
+// portfolio performance
+export type TopPortfolio = {
+    'Rank': number | string;
+    'Username': string;
+    'Portfolio Value': number;
+    'Change (%)': number;
+    'Portfolio Age (days)': number;
+    'Daily Change (%)': number;
+}
+
+// portfolio daily performance
+export type DailyPortfolio = {
+    'Rank': number | string;
+    'Username': string;
+    'Change (%)': number;
+    'Change ($)': number;
+    'Portfolio Value': number;
+}
