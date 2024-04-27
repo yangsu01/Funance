99e9ab865c4f21e99acc018a3f149132f25d8d0a
yangsu01 <ssgc1101@gmail.com>
2024-04-27 14:02:37 -0700

    build: turn off debug mode

--- server/api.py
+++ server/api.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_apscheduler import APScheduler
@@ -122,4 +122,4 @@ if __name__ == '__main__':
     with api.app_context():
         db.create_all()
 
-    api.run(debug=os.getenv('DEV_ENV', False), port=os.getenv('PORT', default=5000))
+    api.run(debug=False, port=os.getenv('PORT', default=5000))
