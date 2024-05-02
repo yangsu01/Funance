2297269d93042b3100ccd73ca12615d4ac362a0f
yangsu01 <ssgc1101@gmail.com>
2024-05-01 18:14:38 -0700

    style: update styling for list groups

--- client/src/components/UI/InfoList.tsx
+++ client/src/components/UI/InfoList.tsx
@@ -2,15 +2,34 @@ import { ListGroup } from "react-bootstrap";
 
 type Props = {
   items: string[];
+  links?: string[];
+  breakpoint?: "sm" | "md" | "lg" | "xl";
 };
 
-const InfoList = ({ items }: Props) => {
+const InfoList = ({ items, links, breakpoint }: Props) => {
   return (
-    <ListGroup>
-      {items.map((item, index) => (
-        <ListGroup.Item key={index}>{item}</ListGroup.Item>
-      ))}
-    </ListGroup>
+    <>
+      {links ? (
+        <ListGroup horizontal={breakpoint}>
+          {items.map((item, index) => (
+            <ListGroup.Item
+              key={index}
+              action
+              href={links[index]}
+              target="_blank"
+            >
+              {item}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      ) : (
+        <ListGroup horizontal={breakpoint}>
+          {items.map((item, index) => (
+            <ListGroup.Item key={index}>{item}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
+    </>
   );
 };
 
