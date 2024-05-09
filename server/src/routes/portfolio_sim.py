b57a598b5e2635495e8c6da08bea63198a81a4bb
yangsu01 <ssgc1101@gmail.com>
2024-05-08 21:49:18 -0700

    refactor: update routes

--- server/src/routes/portfolio_sim.py
+++ server/src/routes/portfolio_sim.py
@@ -2,17 +2,10 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, current_user
 from datetime import datetime
 
-from ..utils.portfolio_sim_functions import (
-    # validation
-    check_game_exists, check_game_name_exists, check_game_password, check_portfolio_exists,
-    # modifying database data
-    add_game, add_portfolio, add_stock, record_transaction,
-    # getting data from database
-    get_games_list, get_game_leaderboard, get_latest_portfolio_id, get_portfolio, get_stock_id,
-    get_buy_info, get_sell_info
-
-)
-from ..utils.yf_functions import get_stock_info, get_stock_news, get_stock_history
+from src.utils.game import add_game, add_portfolio, get_games_list, get_game_leaderboard
+from src.utils.portfolio import get_latest_portfolio_id, get_portfolio
+from src.utils.stock_data import get_stock_info, get_stock_news, get_stock_history
+from src.utils.transaction import add_stock, record_transaction, get_buy_info, get_sell_info
 
 
 portfolio_sim = Blueprint('portfolio_sim', __name__)
@@ -40,14 +33,13 @@ def create_game():
     fee_type = request.json.get('feeType', None)
 
     creator_id = current_user.id
-    
     start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
     end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date() if end_date_str is not None else None
-
-    if check_game_name_exists(name):
-        return jsonify(msg='Game name already taken!'), 409
     
     try:
+        transaction_fee = transaction_fee/100 if fee_type == 'Percentage' else transaction_fee
+        fee_type = 'Flat Fee' if fee_type == "" else fee_type
+        
         game_id = add_game(
             creator_id,
             name,
@@ -55,11 +47,11 @@ def create_game():
             start_date,
             end_date,
             starting_cash,
-            transaction_fee/100 if fee_type == 'Percentage' else transaction_fee,
-            fee_type if fee_type != "" else 'Flat Fee'
+            transaction_fee,
+            fee_type
         )
 
-        portfolio_id = add_portfolio(game_id, creator_id)
+        portfolio_id = add_portfolio(game_id, creator_id, password)
 
     except Exception as e:
         print (str(e))
@@ -77,22 +69,14 @@ def join_game():
     '''Join an existing game
 
         args:
-            gameName (str): name of the game
+            gameId (str): id of the game
             gamePassword (str): password for the game (optional)
     '''
-    name = request.json.get('gameName', None)
+    game_id = request.json.get('gameId', None)
     password = request.json.get('gamePassword', None)
 
-    game = check_game_password(name, password)
-
-    if game == -1:
-        return jsonify(msg='Incorrect Password!'), 401
-
-    user_id = current_user.id
-    game_id = game
-
     try:
-        portfolio_id = add_portfolio(game_id, user_id)
+        portfolio_id = add_portfolio(game_id, current_user.id, password)
     except Exception as e:
         return jsonify(msg=str(e)), 400
 
@@ -125,13 +109,10 @@ def game_leaderboard(game_id: str):
     '''
     game_id = int(game_id)
 
-    if not check_game_exists(game_id):
-        return jsonify(msg='Game does not exist!'), 404
-
     try:
         data=get_game_leaderboard(game_id, current_user.id)
 
-    except ValueError as e:
+    except Exception as e:
         return jsonify(msg=str(e)), 400
 
     return jsonify(
@@ -142,7 +123,7 @@ def game_leaderboard(game_id: str):
 
 @portfolio_sim.route('/portfolio/<portfolio_id>', methods=['GET'])
 @jwt_required()
-def my_portfolio(portfolio_id: str):
+def portfolio(portfolio_id: str):
     '''Get the portfolio summary for a user
         if portfolio_id is -1, return the latest portfolio that was created
 
@@ -150,17 +131,11 @@ def my_portfolio(portfolio_id: str):
             portfolio_id (int): id of the portfolio
     '''
     portfolio_id = int(portfolio_id)
-
-    if portfolio_id == -1:
-        portfolio_id = get_latest_portfolio_id(current_user.id)
-
-        if portfolio_id is None:
-            return jsonify(msg='You do not have any portfolios!'), 404
-        
-    elif not check_portfolio_exists(portfolio_id, current_user.id):
-        return jsonify(msg='Portfolio does not exist!'), 404
-        
+    
     try:
+        if portfolio_id == -1:
+            portfolio_id = get_latest_portfolio_id(current_user.id)
+            
         data = get_portfolio(current_user.id, portfolio_id)
 
     except Exception as e:
@@ -180,10 +155,15 @@ def stock_info(ticker: str):
         args:
             ticker (str): stock ticker (uppercase)
     '''
+    ticker = ticker.upper()
+    
     try:
         stock_info = get_stock_info(ticker)
         news = get_stock_news(ticker)
         history = get_stock_history(ticker, '1y')
+        
+        # add stock to database
+        _ = add_stock(ticker, stock_info['price'])
 
     except Exception as e:
         return jsonify(msg=str(e)), 400
@@ -197,6 +177,7 @@ def stock_info(ticker: str):
         msg="success"
     ), 200
 
+
 @portfolio_sim.route('/buy-info/<portfolio_id>', methods=['GET'])
 @jwt_required()
 def buy_info(portfolio_id: str):
@@ -207,13 +188,15 @@ def buy_info(portfolio_id: str):
     '''
     portfolio_id = int(portfolio_id)
 
-    if check_portfolio_exists(portfolio_id, current_user.id):
-        return jsonify(
-            data=get_buy_info(portfolio_id),
-            msg="success"
-        ), 200
-    else:
-        return jsonify(msg='Portfolio does not exist!'), 404
+    try:
+        data = get_buy_info(portfolio_id, current_user.id)
+    except Exception as e:
+        return jsonify(msg=str(e)), 400
+    
+    return jsonify(
+        data=data,
+        msg="success"
+    ), 200
         
 
 @portfolio_sim.route('/sell-info/<portfolio_id>', methods=['GET'])
@@ -226,40 +209,33 @@ def sell_info(portfolio_id: str):
     '''
     portfolio_id = int(portfolio_id)
 
-    if check_portfolio_exists(portfolio_id, current_user.id):
-        return jsonify(
-            data=get_sell_info(portfolio_id),
-            msg="success"
-        ), 200
-    else:
-        return jsonify(msg='Portfolio does not exist!'), 404
-
-
+    try:
+        data = get_sell_info(portfolio_id, current_user.id)
+    except Exception as e:
+        return jsonify(msg=str(e)), 400
+    
+    return jsonify(
+        data=data,
+        msg="success"
+    ), 200
 
 
 @portfolio_sim.route('/buy-stock', methods=['POST'])
 @jwt_required()
 def buy_stock():
     '''Buy a stock
-        assumes transaction can be afforded (validation in frontend)
     
         args:
             portfolioId (int): id of the portfolio
-            ticker (str): stock ticker
+            stockId (int): id of the stock
             shares (int): number of shares
-            price (float): price per share
     '''
     portfolio_id = request.json.get('portfolioId', None)
-    ticker = request.json.get('ticker', None)
-    shares = int(request.json.get('shares', None))
-    price = float(request.json.get('price', None))
-
-    if not check_portfolio_exists(portfolio_id, current_user.id):
-        return jsonify(msg='Portfolio does not exist!'), 404
+    stock_id = request.json.get('stockId', None)
+    shares = request.json.get('shares', None)
 
     try:
-        stock_id = add_stock(ticker, price)
-        record_transaction(portfolio_id, stock_id, 'buy', shares, price)
+        record_transaction(portfolio_id, stock_id, 'buy', shares)
 
     except Exception as e:
         return jsonify(msg=str(e)), 400
@@ -271,25 +247,18 @@ def buy_stock():
 @jwt_required()
 def sell_stock():
     '''Sell a stock
-        assumes transaction can be afforded (validation in frontend)
     
         args:
             portfolioId (int): id of the portfolio
-            ticker (str): stock ticker (uppercase)
+            stockId (int): id of the stock
             shares (int): number of shares
-            price (float): price per share
     '''
     portfolio_id = request.json.get('portfolioId', None)
-    ticker = request.json.get('ticker', None)
-    shares = int(request.json.get('shares', None))
-    price = float(request.json.get('price', None))
-
-    if not check_portfolio_exists(portfolio_id, current_user.id):
-        return jsonify(msg='Portfolio does not exist!'), 404
+    stock_id = request.json.get('stockId', None)
+    shares = request.json.get('shares', None)
 
     try:
-        stock_id = get_stock_id(ticker)
-        record_transaction(portfolio_id, stock_id, 'sell', shares, price)
+        record_transaction(portfolio_id, stock_id, 'sell', shares)
 
     except Exception as e:
         return jsonify(msg=str(e)), 400
