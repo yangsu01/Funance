ebe39884cd168821517ca5e601a4d30ce2ef1b7d
yangsu01 <ssgc1101@gmail.com>
2024-05-10 16:54:23 -0700

    build: remove vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
