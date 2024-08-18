49e6a51926d2c3745aa0b2963bb366c5d5c131bd
yangsu01 <ssgc1101@gmail.com>
2024-08-18 15:43:29 -0700

    add: checks for holidays

--- server/src/utils/time.py
+++ server/src/utils/time.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pytz
 import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar
 
 
 def get_est_time() -> datetime:
@@ -29,26 +30,27 @@ def utc_to_est(utc_time: datetime) -> datetime:
     return utc_time.astimezone(est)
 
 
-def check_market_closed() -> bool:
+def check_market_closed(date: datetime = get_est_time() ) -> bool:
     """ Checks if the stock market is closed
         Assumes market is closed on weekends and after 4pm EST
 
     Returns:
         bool: True if market is closed, False if market is open
     """
-    est_time = get_est_time()
+    cal = USFederalHolidayCalendar()
+    holidays = cal.holidays(start=date.date(), end=date.date()+pd.DateOffset(years=1))
     
-    if est_time.weekday() > 5:
+    if date.weekday() >= 5 or date.date() in holidays:
         return True
-    elif est_time.hour >= 16: 
+    elif date.hour >= 16: 
         return True
-    elif est_time.hour < 9 or (est_time.hour == 9 and est_time.minute < 30):
+    elif date.hour < 9 or (date.hour == 9 and date.minute < 30):
         return True
     
     return False
     
 
-def get_market_date(est_time: datetime) -> datetime.date:
+def get_prev_market_date(est_time: datetime) -> datetime.date:
     """ Gets the last day the stock market was open
         If the market is currently open, returns the current date
     
@@ -58,7 +60,38 @@ def get_market_date(est_time: datetime) -> datetime.date:
     Returns:
         datetime.date: date of last market day
     """
-    if est_time.weekday() < 5 and est_time.hour >= 10:
-        return est_time.date()
+    cal = USFederalHolidayCalendar()
+    holidays = cal.holidays(start=est_time.date(), end=est_time.date()+pd.DateOffset(years=1))
+    
+    # if its a weekend or holiday, subtract a day
+    if est_time.weekday() >= 5 or est_time.date() in holidays:
+        return get_prev_market_date(est_time - pd.DateOffset(days=1))
+    # if its before 9:30am (market has not opened yet), subtract a day
+    elif est_time.hour < 9 or (est_time.hour == 9 and est_time.minute < 30):
+        return get_prev_market_date(est_time - pd.DateOffset(days=1))
     else:
-        return est_time.date() - pd.tseries.offsets.BDay(1)
+        return est_time.date()
+    
+
+def get_next_market_date(est_time: datetime=get_est_time()) -> str:
+    """ Gets the next day the stock market will be open
+        If the market is currently open, returns the current date
+        If the market has not opened yet, returns the current date
+    
+    Args:
+        est_time (datetime): time in EST
+
+    Returns:
+        str: date of next market day
+    """
+    cal = USFederalHolidayCalendar()
+    holidays = cal.holidays(start=est_time.date(), end=est_time.date()+pd.DateOffset(years=1))
+    
+    # if its a weekend or holiday, add a day
+    if est_time.weekday() >= 5 or est_time.date() in holidays:
+        return get_next_market_date(est_time.date() + pd.DateOffset(days=1))
+    # if its after 4pm, add a day
+    elif est_time.hour >= 16:
+        return get_next_market_date(est_time.date() + pd.DateOffset(days=1))
+    else:
+        return est_time.date().strftime('%Y-%m-%d')
