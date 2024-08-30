74f14d5267e1d49179b6785261b15e8d85a2021f
yangsu01 <ssgc1101@gmail.com>
2024-08-30 15:07:18 -0700

    Revise version to v1.3.1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   // app version
-  const version = "v1.2.1";
+  const version = "v1.3.1";
 
   return (
     <AuthProvider>
